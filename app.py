3761dce337acc5a4a4453ad886e7a5243b6a8dd0
Nilesh123786512 <nikileshthotamsettyhrank@gmail.com>
2025-03-14 16:16:22 +0000

    Created ai chatbot

---
+++ app.py
@@ -0,0 +1,175 @@
+import asyncio
+from utils import query_openai, models_dict
+import streamlit as st
+import re
+import time
+models_dict_reversed = {value: key for key, value in models_dict.items()}
+
+
+
+# Initialize session state for conversation if not already present
+if "conversation" not in st.session_state:
+    st.session_state.conversation = [{
+        "role": "system",
+        "content": "You are a helpful assistant."
+    }]
+if "input" not in st.session_state:
+    st.session_state.input = [{
+        "role": "system",
+        "content": "You are a helpful and concise assistant. Your primary goal is to directly address the user's most recent question or statement.  Use the preceding conversation history to understand the context and provide relevant background, but always ensure your response is primarily focused on and answers the *latest user input*.  If there are any ambiguities or contradictions between past messages and the latest message, assume the latest message is the most accurate representation of the user's current intent."
+    }]
+
+with st.sidebar:
+    st.session_state.temp = st.slider("Temperature",0., 2.,0.75)
+    st.session_state.top_p = st.slider("Top_p",0., 1.,0.95)
+    
+    
+    # st.title("Example: Controlling sidebar programmatically")
+# Model selection dropdown
+# model = st.selectbox("Select a model:", list(models_dict.values()))
+st.title("My Chatbot")
+
+
+hist_container = st.container()
+with hist_container:
+    for chat in st.session_state.conversation:
+
+        if chat["role"] == "user":
+            user_msg = st.chat_message("user")
+            # st.markdown("### You")
+            user_msg.text(f"{chat['content']}")
+        elif chat["role"] == "assistant":
+            thoughts_match = re.search(r'''<think>(.*?)</think>''',
+                                       chat['content'], re.DOTALL)
+            main_content = re.sub(r'''<think>.*?</think>''',
+                                  '',
+                                  chat['content'],
+                                  flags=re.DOTALL)
+            # st.markdown("### Bot")
+            if thoughts_match:
+                thoughts = thoughts_match.group(1)
+                thoughts = re.sub(r"\\\[\s*", "$$", thoughts)
+                thoughts = re.sub(r"\s*\\\]", "$$", thoughts)
+                thoughts = re.sub(r"\\\(\s*", "$", thoughts)
+                thoughts = re.sub(r"\s*\\\)", "$", thoughts)
+                with st.expander("Thoughts"):
+                    st.write(thoughts)
+            # print(main_content)
+            # print(main_content)
+    # main_content = re.sub(r"\\\[\s(.*?)\s\\\]", "$$\1$$", main_content)
+            # main_content = re.sub(r"\\\[\s*", "$$", main_content)
+            main_content = re.sub(r"\\\(\s*", "$", main_content)
+            main_content = re.sub(r"\s*\\\)", "$", main_content)
+            bot_message= st.chat_message("assistant")
+            # main_content = re.sub(r"\s*\\\]", "$$", main_content)
+            latex_blocks = re.findall(r"\\\[(.*?)\\\]", main_content, re.DOTALL)
+
+            # Split text around LaTeX expressions
+            split_text = re.split(r"\\\[(.*?)\\\]", main_content, flags=re.DOTALL)
+
+            # Render content correctly
+            for i in range(len(split_text)):
+                bot_message.markdown(split_text[i])  # Render text normally
+                if i < len(latex_blocks):  
+                    bot_message.latex(latex_blocks[i].strip()) 
+            # print(main_content)
+            # bot_message.markdown(main_content)
+
+
+
+new_reply_container = st.container()
+
+main_container = st.container()
+with main_container:
+    # Use columns only for the selectbox (chat input stays outside)
+    col1, col3, _, col2 = st.columns(
+        [1, 2, 1, 1],
+        vertical_alignment="bottom"
+    )  # Underscore ignores the second column
+
+    with col3:
+        model = st.selectbox("Select a Model", list(models_dict.values()))
+    with col2:
+        # "New Chat" button on the right
+        if st.button("New Chat"):
+            st.session_state.conversation = [{
+                "role":
+                "system",
+                "content":
+                "You are a helpful assistant."
+            }]
+            st.session_state.input = [{
+                "role": "system",
+                "content": "You are a helpful assistant."
+            }]
+            st.rerun()
+    with col1:
+        search = st.toggle("Search")
+
+    user_message = st.chat_input("Type your message here...")
+
+if user_message:
+    st.session_state.conversation.append({
+        "role": "user",
+        "content": user_message
+    })
+    st.session_state.input.append({"role": "user", "content": user_message})
+    with new_reply_container:
+        # Append user message only if it exists.
+        # st.markdown("### You"
+        user_msg = st.chat_message("user")
+        user_msg.text(user_message)
+
+        # bot_message.write('thinking')
+
+        model_number = models_dict_reversed.get(model)
+        # Set think flag based on model_number.
+        if model_number in [2, 3, 4]:
+            st.session_state.think = True
+        else:
+            st.session_state.think = False
+
+        # Query the model
+        ass_message_place = st.empty()
+        with ass_message_place.container():
+
+            # Define the CSS for the blinking effect
+            blink_css = """
+            <style>
+            @keyframes blink {
+                0% { opacity: 1; }
+                50% { opacity: 0; }
+                100% { opacity: 1; }
+            }
+            .blink {
+                animation: blink 1s infinite;
+            }
+            </style>
+            """
+
+            # Apply the CSS
+            st.markdown(blink_css, unsafe_allow_html=True)
+
+            # Display blinking text
+            # st.markdown('<p class="blink">This text will blink</p>', unsafe_allow_html=True)
+
+            st.markdown('<p class="blink">Thinking</p>',
+                        unsafe_allow_html=True)
+
+        # print("all ready to send to AI")
+        response =asyncio.run(query_openai(st.session_state.input,
+                                model_number,
+                                search=search,
+                             temp=st.session_state.temp,
+                              top_p=st.session_state.top_p)
+                             )
+        # if response[1] is not None:
+        #     st.session_state.input.append({
+        #         "role": "system",
+        #         "content": f"The search results provided for context {response[1]}"
+        # })
+        st.session_state.conversation.append({
+            "role": "assistant",
+            "content": response
+        })
+        st.rerun()
---
+++ utils.py
@@ -0,0 +1,111 @@
+import os
+from openai import OpenAI
+from tavily import TavilyClient
+import asyncio
+import re
+
+models_dict = {
+    0: "qwen-qwq-32b",
+    1: "qwen-2.5-32b",
+    2: "qwen-2.5-coder-32b",
+    3: "deepseek-r1-distill-qwen-32b",
+    4: "deepseek-r1-distill-llama-70b",
+    5: "deepseek-r1",
+    6: "deepseek-v3",
+    7: "claude-3-5-sonnet-20240620",
+    8: "gpt-4o-2024-05-13"
+}
+
+## Declaring the clients for different purposes
+client1 = OpenAI(
+    # base_url="https://api.groq.com/openai/v1",
+    base_url="https://api.sree.shop/v1",
+    # api_key=os.environ['groq_api']
+    api_key=os.environ['devsdocode'])
+client2 = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    # base_url="https://api.sree.shop/v1",
+    api_key=os.environ['groq_api']
+    # api_key=os.environ['devsdocode']
+)
+client3 = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    # base_url="https://api.sree.shop/v1",
+    api_key=os.environ['groq_api2']
+    # api_key=os.environ['devsdocode']
+)
+tavily_client = TavilyClient(api_key=os.environ['tavily_1'])
+
+
+#Tavily search if asked
+async def search_tavily(query):
+    # print(f'Got query {query}')
+    try:
+        query = query.copy()
+        query.append({
+            "role":
+            "system",
+            "content":
+            "Based on all above queries give me a single search query enclosed in <search>content to search</search> that is relevant to collect information about the topic"
+        })
+
+        # print(f"Changed query:{query}")
+        # query_filtered.choices[0].message.content
+        query_filtered = client3.chat.completions.create(
+            model="llama3-70b-8192", messages=query)
+        query_filtered = re.search(r'''<search>(.*?)</search>''',                                query_filtered.choices[0].message.content,
+                                   re.DOTALL)
+        # print(query_filtered)
+        response = tavily_client.search(query_filtered.group(1))
+        # print('Requested the query from tavily')
+        return response['results'][0]['content']
+    except Exception as e:
+        print(f'Exception is {e}')
+        return "Unable to fetch search results"
+
+
+# openai.api_key = "your_openai_api_key"  # Replace with your actual OpenAI API key
+async def query_openai(conversation,
+                       model_number=1,
+                       search=False,
+                       temp=0.5,
+                       top_p=0.95):
+    """
+    Send the conversation history to the OpenAI Chat API and return the assistant's response.
+    """
+    respo = None
+    if search:
+        print(conversation[-1]['content'])
+        respo = await search_tavily(conversation)
+        print(f'response is provided by tavily {respo}')
+        conversation.append({
+            "role":
+            "system",
+            "content":
+            f"Additional context from recent search results: {respo}"
+        })
+    try:
+        if model_number in [5, 6, 7, 8]:
+            print(f"Conversation :{conversation}")
+            response = client1.chat.completions.create(
+                model=models_dict[model_number],
+                messages=conversation,
+                temperature=temp,  # Set temperature to 0.7
+                top_p=top_p)
+            # print(f"Response is {response.choices[0].message.content}")
+        else:
+            response = client2.chat.completions.create(
+                model=models_dict[model_number], messages=conversation)
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        raise e
+        return f"Error: {e}"
+
+
+
+
+
+
+
+
+        
