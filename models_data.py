06012291e7044293e74847a6c9b3fc0c2f4d192e
Nilesh123786512 <nikileshthotamsettyhrank@gmail.com>
2025-07-17 22:38:40 +0530

    MODEL UPDATE: updated model from different providers

--- models_data.py
+++ models_data.py
@@ -1,24 +1,24 @@
 models_dict = {
     8: "gemini-2.5-flash-lite-preview-06-17",
     19: "gemini-2.5-flash",
-    20: "provider-6/gpt-4o",
+    20: "provider-6/o3-high",
     12: "openai/gpt-4.1-mini",
     11: "gemini-2.5-pro",
     9: "deepseek/deepseek-chat:free",
     17: "gemini-2.5-flash-preview-05-20",
     7: "provider-6/o3-low",
     5: "provider-3/deepseek-v3",
-    6: "provider-3/deepseek-v3-0324",
-    14: "provider-1/gemini-2.5-pro",
+    6:  "provider-3/deepseek-v3-0324",
+    14: "provider-6/kimi-k2",
     24: "provider-6/deepseek-r1-uncensored",
-    25:"provider-1/deepseek-r1-0528",
-    16: "provider-5/o3-low",
-    18: "provider-3/claude-3.5-haiku",
+    25: "provider-1/deepseek-r1-0528",
+    16: "provider-1/claude-3.7-sonnet-thinking",
+    18: "provider-1/claude-sonnet-4",
     21: "provider-6/o3-medium",
     22: "provider-3/gpt-4.1-mini",
     23: "provider-6/gpt-4.1",
-    1: "gemini-2.0-flash-thinking-exp-1219",
-    3: "openai/gpt-4.1",
+    1:  "gemini-2.0-flash-thinking-exp-1219",
+    3:  "openai/gpt-4.1",
     13: "deepseek/deepseek-chat-v3-0324:free",
     15: "meta-llama/llama-4-maverick:free",
     2: "nvidia/llama-3.1-nemotron-ultra-253b-v1:free",
@@ -35,10 +35,10 @@ bot_icon_url=["icons/gemini.png",
 user_icon_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGWm7kgMH1PEsycRwkyqPcPB1b2NITpD8j2g&s"
 
 
-gemini_model_numbers=[11,14,17,1,8,19]
-chatgpt_model_numbers=[12,3,20,21,22,16,23]
-deepseek_model_numbers=[13,9,25,4,10,5,6,7,24]
-claude_model_numbers=[18]
+gemini_model_numbers=[11,17,1,8,19]
+chatgpt_model_numbers=[12,3,20,21,22,23,7]
+deepseek_model_numbers=[13,9,25,4,10,5,6,24]
+claude_model_numbers=[18,16]
 
 
 def get_icon_no_and_value(model_number):
